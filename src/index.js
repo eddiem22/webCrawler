967a2042f33ccc1e29011a5cde46b36b92f059d2
Eddie <marchinthecov@yahoo.com>
2022-05-24 10:31:24 -0400

    fixed error handling

--- src/index.js
+++ src/index.js
@@ -2,17 +2,16 @@ const cheerio = require('cheerio');
 const fs = require('fs');
 const path = require('path');
 const urlParser = require('url');
-const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
-
 const visitedURLS = {};
 
-const getURL = (link, host, protocol) => {
+const getURL = async(link, host, protocol) => {
     if(link.includes("http")) return link;
     else if(link.startsWith("/")) return `${protocol}//${host}/${link}`;
     else return `${protocol}//${host}/${link}`;
 };
 
 const crawl = async({url, ignore}) => {
+    try{
     if(visitedURLS[url]) return;
     console.log("crawing page: ", url);
     visitedURLS[url]  = true;
@@ -31,23 +30,29 @@ const crawl = async({url, ignore}) => {
     .get();
 
     imageLinks.forEach((imageURL) => {
-        fetch(getURL(imageURL, host, protocol)).then((response) => {
+         getURL(imageURL, host, protocol).then((URL) => {
+         fetch(URL).then((response) => {
             const filename = path.basename(imageURL)
             const dest = fs.createWriteStream(`images/${filename}`)
-            response.body.pipe(dest)
+            //response.body.pipe(dest)
         });
+        })
     }); 
 
     links.filter((link) => link.includes(host) && !link.includes(ignore))
-    .forEach((link) => {
-        crawl({url: getURL(link, host, protocol), 
+    .forEach(async(link) => {
+        await getURL(link, host, protocol).then((URL) => {
+         crawl({url: URL,
             ignore,
-        });
+        })})
     });
-};
+}
+catch(e){console.log(e);}
+}
+
 
 
 crawl({
-    url: "https://www.amazon.com/",
+    url: "https://www.reddit.com/",
     ignore: "/search",
-});
+})
